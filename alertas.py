11f4716b5fd5321aeac8197d31d6f00ad95dc19f
jhonholberton <3425@holbertonschool.com>
2022-04-28 03:20:30 -0500

    envio de alertas

---
+++ alertas.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""envio de alertas"""
+from tkinter import Frame, Label, Tk, BOTH, YES, mainloop
+
+
+class alerta():
+    """clase que contiene los contenedores y metodos para enviar alertas"""
+
+    def __init__(self, mensaje):
+        """constructor"""
+        self.inicio = Tk()
+        self.inicio.title("Aviso")
+        self.mensaje = mensaje
+
+    def contenedor(self):
+        """contenedores y metodos para dibujar la ventana de alertas"""
+        self.micontenedor = Frame(self.inicio)
+        self.micontenedor.pack(expand=YES, fill=BOTH)
+        self.etiqueta1 = Label(
+            self.micontenedor, text=self.mensaje, font=("Arial", 28))
+        self.etiqueta1.pack()
+        self.inicio.mainloop()
